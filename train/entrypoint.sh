ab4fbc68be597f72808869e3d9ad2b673a377dc5
woo4826_106 <woo4826@gmail.com>
2024-05-01 21:35:21 +0900

    로라에에포크크, repeat, batch 수정

--- train/entrypoint.sh
+++ train/entrypoint.sh
@@ -59,7 +59,7 @@ echo "[[datasets]]" >> "$DATASET_CONFIG_FILE"
 echo "[[datasets.subsets]]" >> "$DATASET_CONFIG_FILE"
 echo "image_dir = '/workspace/uploads/$FOLDER_KEY'" >> "$DATASET_CONFIG_FILE"
 echo "caption_extension = '.txt'" >> "$DATASET_CONFIG_FILE"
-echo "num_repeats = 10" >> "$DATASET_CONFIG_FILE"
+echo "num_repeats = 20" >> "$DATASET_CONFIG_FILE"
 
 source activate conda
 
@@ -68,9 +68,9 @@ accelerate launch --num_cpu_threads_per_process 2 train_network.py \
   --output_dir="/workspace/output/$FOLDER_KEY" \
   --output_name="$OUTPUT_NAME" \
   --dataset_config="/workspace/train/dc_$FOLDER_KEY.toml" \
-  --train_batch_size=1 \
-  --max_train_epochs=5 \
-  --resolution="768,768" \
+  --train_batch_size=2 \
+  --max_train_epochs=20 \
+  --resolution="512,512" \
   --optimizer_type="AdamW8bit" \
   --learning_rate=1e-4 \
   --network_dim=32 \
